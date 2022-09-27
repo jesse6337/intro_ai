ed55470f77c5c7eea7c32f9d855938621ca3b5ea
Jesse <jesseramos6337@gmail.com>
2022-09-27 10:04:02 -0400

    finished finding prob

--- grid.py
+++ grid.py
@@ -11,12 +11,20 @@ wLen = 1/len(world)
 belief = [wLen for i in range(len(world))]
 # Intial prob belief of robot being in any position on the map 
 # (descrete uniform distrobution)
-
+currentSenorReading = 'g'
+for i in range(len(world)):
+    if currentSenorReading == world[i]:
+        belief[i] *= 0.9 
+    elif currentSenorReading != world[i]:
+        belief[i] *= 0.1 
+sum = 0 
+for i in belief:
+    sum += i
+for i in range(len(belief)):
+    belief[i] /= sum
 #robot makes sence of envirment
 #sensing green is the same as not sensing red
-currentSenorReading = 'g'
 
 #upade prob belief of robot's position in the world(map)
 print(wLen)
 print(belief)
-#dsa
