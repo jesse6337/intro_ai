db88c0ff41c909843fcbfd4dd1605fc308a29c52
Jesse <jesseramos6337@gmail.com>
2022-09-28 13:25:20 -0400

    end of class 9.28

---
+++ map.py
@@ -0,0 +1,23 @@
+# 1 is door 0 is wall
+world = [1,0,1,0,0,1,1,0]
+belief = []
+# P(Xi)
+for i in range(len(world)):
+    belief.append(1/len(world))
+sAcc = 0.83
+sWrong = 0.17
+currentSensorReading = 0
+normalizor = 0
+# then * P(S| Xi)
+for i in range(len(belief)):
+    if currentSensorReading == world[i]:
+        belief[i] *= sAcc
+    else: belief[i] *= sWrong
+#normalizer
+for i in belief:
+    normalizor += i
+#complete equation
+for i in range(len(belief)):
+    belief[i] /= normalizor
+for i in belief:
+    print(i)
