05cb77c1acdcc028fa90f73c782905ab4554defe
Jesse <jesseramos6337@gmail.com>
2022-11-17 19:46:50 -0500

    2d works

--- Q1/2d.py
+++ Q1/2d.py
@@ -25,8 +25,7 @@ def sense(map, belief, reading):
         for j in range(len(belief[i])):
             belief[i][j] /= normilizer
     return belief
-def bay(belief1, belief2):
-    normilizer =0
+def average(belief1, belief2):
     belief = []
     for i in range(len(belief1)):
         belief.append([])
@@ -34,15 +33,9 @@ def bay(belief1, belief2):
             belief[i].append(0)
     for i in range(len(belief)):
         for j in range(len(belief[i])):
-            belief[i][j] = belief1[i][j]*belief2[i][j]
-    for i in range(len(belief2)):
-        for j in range(len(belief2[i])):
-            normilizer += belief[i][j]
-    for i in range(len(belief)):
-        for j in range(len(belief[i])):
-            belief[i][j] /= normilizer
+            belief[i][j] = (belief1[i][j]+belief2[i][j])/2
     return belief
-    #still in progress
+    #This is the problem
 
 def move(world, belief, move_amount):
     moveBelief = []
@@ -51,17 +44,23 @@ def move(world, belief, move_amount):
         for j in range(len(world[i])):
             moveBelief[i].append(0)
     # move in directions
-    directionx = move_amount[0]/abs(move_amount[0])
-    directiony = move_amount[1]/abs(move_amount[0])
+    if move_amount[0] == 0:
+        directionx = 0
+    else:
+        directionx = move_amount[0]/abs(move_amount[0])
+    if move_amount[1] == 0:
+        directiony = 0
+    else:
+        directiony = move_amount[1]/abs(move_amount[1])
     for i in range(len(moveBelief)):
         # move in x direction 
         for j in range(len(moveBelief[i])):
-            unShootIx = int(move_amount[0]+i - directionx) % len(world) 
-            acShootIx = (move_amount[0]+i) % len(world) 
-            ovShootIx = int(move_amount[0]+i + directionx) % len(world)
-            moveBelief[i][unShootIx] += belief[i][j] * 0.1
-            moveBelief[i][acShootIx] += belief[i][j] * 0.8
-            moveBelief[i][ovShootIx] += belief[i][j] * 0.1
+            unShootIx = int(move_amount[0]+j - directionx) % len(world) 
+            acShootIx = (move_amount[0]+j) % len(world) 
+            ovShootIx = int(move_amount[0]+j + directionx) % len(world)
+            moveBelief[i][unShootIx] += (belief[i][j] * 0.1)
+            moveBelief[i][acShootIx] +=( belief[i][j] * 0.8)
+            moveBelief[i][ovShootIx] += (belief[i][j] * 0.1)
     moveBelief2 = []
     for i in range(len(world)):
         moveBelief2.append([])
@@ -70,16 +69,22 @@ def move(world, belief, move_amount):
         # move in y direction
     for i in range(len(belief[0])):
         for j in range(len(belief)):
+            #un index is currentsly an issue
             unShootIy = int(move_amount[1]+i - directiony) % len(world) 
             acShootIy = (move_amount[1]+i) % len(world) 
-            ovShootIy = int(move_amount[1]+i + directiony) % len(world)
-            moveBelief2[unShootIy][j] += belief[j][i] * 0.1
-            moveBelief2[acShootIy][i] += belief[j][i] * 0.8
-            moveBelief2[ovShootIy][i] += belief[j][i] * 0.1
-    return bay(moveBelief, moveBelief2)
-belief = sense(map, belief, 'r')
-print(belief)
-belief = move(map, belief, [2,0])
-print("")
-sum = 0
-print(belief)
+            ovShootIy = int((move_amount[1]+i) + directiony) % len(world)
+            #print(unShootIy)
+            #print(acShootIy)
+            #print(ovShootIy)
+            moveBelief2[unShootIy][j] += (belief[i][j] * 0.1)
+            moveBelief2[acShootIy][j] += (belief[i][j] * 0.8)
+            moveBelief2[ovShootIy][j] += (belief[i][j] * 0.1)
+    return average(moveBelief, moveBelief2)
+readings = ['r','r','g', 'r', 'r']
+moves = [[1,0], [1,0], [1,0], [1,0], [1,0]]
+for i in range(len(readings)):
+    belief = sense(map, belief, readings[i])
+    print(belief)
+    belief = move(map, belief, moves[i])
+    print("")
+    print(belief)
