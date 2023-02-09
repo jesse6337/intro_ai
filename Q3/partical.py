79ab2480f717ffac394123da61366ba7ebd980df
Jesse <jesseramos6337@gmail.com>
2023-02-09 13:10:53 -0500

    help

--- Q3/partical.py
+++ Q3/partical.py
@@ -1,15 +1,42 @@
 import random
+import statistics
+
 ### Guess format
-#  [random x, random y, distance in meters, importance]
+#  [random x, distance in meters, importance]
+# recreate so that partical is the object
 class partical:
-    def __init__(self, mapSize):
-        self.mapSizeX = mapSize[0]
-        self.mapSizeY = mapSize[1]
+    def __init__(self, map, x, ):
+        self.map = map
+        self.mapSizeX = len(map[0])
+        self.distribution = []
+        self.median = 0
+        self.x = x
+        
+    @staticmethod
+    def show_map(map):
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                print(map[i][j], end = "")
+            print()
     def first_guess(self, numGuess):
         guesses = []
         for i in range(numGuess):
-            guesses.append([random.randrange(0, self.mapSizeX), random.randrange(0, self.mapSizeY),0, 0])
+            guesses.append([random.randrange(0, self.mapSizeX), 0, 0])
         return guesses
-    def sense(reading, guess, error = 1):
-        for i in range(guess):
-            guess[i][3] = -abs(guess[i][2]-reading)
+    # hide height off the ground
+    def find_altitude():
+        pass
+    # assign a importance weight
+    def weight(): 
+        pass
+    
+    def move(self,dX):
+        self.x = (self.x + dX)% self.mapSizeX
+        
+
+    def fuzz():
+        pass
+
+    def __repr__(self):
+        return "{} {}".format(self.x, 0)
+
