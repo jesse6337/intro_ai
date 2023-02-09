e163128abf57174d4945a9597eea4496396431eb
Jesse <jesseramos6337@gmail.com>
2023-02-09 09:31:04 -0500

    in class 2.9

--- algorithums/countConstruct.py
+++ algorithums/countConstruct.py
@@ -1,5 +1,13 @@
 def countConstruct(target, wordBank): # count num of possible combinations
-    pass  
+    if target == '':
+        return 1
+    returnList = 0
+    for i in range(len(wordBank)):
+        if target.startswith(wordBank[i]):
+            part = target.replace(wordBank[i], '', len(wordBank[i]))
+            inner = countConstruct(part, wordBank)
+            returnList += inner
         
+    return returnList
 
-print(countConstruct('purple', ['purp','p','ur','le','purpl']))
+print(countConstruct('enterapotentpot', ['a','p','ent','enter','ot','o','t']))
